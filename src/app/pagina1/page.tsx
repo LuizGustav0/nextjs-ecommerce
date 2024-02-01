1160b0b9c93104f9a9a20ca8031d240efe9c2903
Luiz Gustavo <contato-luiz-gustavo@outlook.com>
2024-02-01 11:59:10 -0300

    feat: create component navbar

---
+++ src/app/pagina1/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material"; 
+
+
+function Pagina1() {
+    return (
+        <div>
+            <h1>Pagina 1</h1>
+            <Button variant="contained" color="primary">sdsds</Button>
+        </div>
+    )
+}
+
+
+export default Pagina1;
